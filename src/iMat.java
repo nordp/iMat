0a62f679c6e075337a73a56f256a78602885a784
kruttekrax <nordp@student.chalmers.se>
2017-02-15 13:34:48 +0100

    Initial commit

---
+++ src/iMat.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Phnor on 2017-02-15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class iMat extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
