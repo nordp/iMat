db6e4d6a3f25c9217b986eba9f55e6a78d6bff9a
Philip <nordp@student.chalmers.se>
2017-02-24 14:25:13 +0100

    Lightbox functionality expanded.

--- src/Controllers/LightboxController.java
+++ src/Controllers/LightboxController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -20,10 +21,12 @@ public class LightboxController implements Initializable{
     @FXML Parent myAccount;
 
     List<Parent> panes;
+    List<Pane> shadows;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         panes = new ArrayList<>();
+        shadows = new ArrayList<>();
         panes.add(history);
         panes.add(shoppingLists);
         panes.add(myAccount);
@@ -54,14 +57,27 @@ public class LightboxController implements Initializable{
         //Toggle the lightbox with parent depending on parent shown
         boolean toggle = !parent.isVisible();
         root.setVisible(toggle);
+        setShadows(toggle);
         parent.setVisible(toggle);
 
     }
 
-    @FXML private void close(){
+    public void close(){
         for (Parent p : panes){
             p.setVisible(false);
         }
         root.setVisible(false);
+        setShadows(false);
     }
+
+    private void setShadows(boolean bool){
+        for (Pane shadow : shadows){
+            shadow.setVisible(bool);
+        }
+    }
+
+    public void addShadow(Pane shadow) {
+        shadows.add(shadow);
+    }
+
 }
--- src/IMatController.java
+++ src/IMatController.java
@@ -2,23 +2,30 @@ import Controllers.LightboxController;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IMatController implements Initializable {
 
     @FXML Parent lightbox;
+    @FXML Pane shadow;
+    @FXML Pane shadow1;
+    @FXML Pane shadow2;
+    @FXML Pane shadow3;
     @FXML LightboxController lightboxController;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         lightbox.setVisible(false);
+        lightboxController.addShadow(shadow);
+        lightboxController.addShadow(shadow1);
+        lightboxController.addShadow(shadow2);
+        lightboxController.addShadow(shadow3);
     }
 
-    @FXML private void myAccountClicked(){
-        lightboxController.myAccount();
-
-    }
+    @FXML private void myAccountClicked(){ lightboxController.myAccount();}
 
     @FXML private void shoppingListsClicked(){
         lightboxController.shoppingLists();
@@ -28,27 +35,18 @@ public class IMatController implements Initializable {
         lightboxController.history();
     }
 
-    @FXML private void toCheckout(){
+    @FXML private void shadowClicked() { lightboxController.close(); }
 
-    }
+    @FXML private void toCheckout(){    }
 
-    @FXML private void toHome(){
-
-    }
+    @FXML private void toHome(){    }
 
     @FXML private void categoryClicked(){ //Bör ta en kategori som indata.
-
     }
 
-    @FXML private void searchPerformed(){
+    @FXML private void searchPerformed(){    }
 
-    }
+    @FXML private void nextPressed(){    }
 
-    @FXML private void nextPressed(){
-
-    }
-
-    @FXML private void backPressed(){
-
-    }
+    @FXML private void backPressed(){    }
 }
